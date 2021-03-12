b3c9b13e215c0a033f0831bfd2439c7e1d1ac114
Casey Hon <casey.hon@mail.utoronto.ca>
2021-03-12 12:56:41 -0500

    lab 8 checkpasswd main

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -32,6 +32,60 @@ int main(void) {
   }
   
   // TODO
+  int fd[2], r;
+  if (pipe(fd) == -1) {
+    perror("pipe");
+    exit(1);
+  } 
+  if ((r = fork()) > 0) {
+    if (close(fd[0]) == -1) { // parent won't be reading from pipe
+      perror("close");
+      exit(1);
+    }
+
+    char *input = strchr(user_id, '\n');
+    *(input + 1) = '\0';
+    if (write(fd[1], user_id, MAX_PASSWORD) == -1) {
+      perror("write");
+      exit(1);
+    }
+
+    input = strchr(password, '\n');
+    *(password + 1) = '\0';
+    if (write(fd[1], password, MAX_PASSWORD) == -1) {
+      perror("write");
+      exit(1);
+    }
+
+    int status;
+    wait(&status);
+    if (WIFEXITED(status)) {
+      if (WEXITSTATUS(status) == 0) {
+        printf(SUCCESS);
+      }
+      else if (WEXITSTATUS(status) == 2) {
+        printf(INVALID);
+      }
+      else if (WEXITSTATUS(status) == 3) {
+        printf(NO_USER);
+      }
+    }
+  }
+  else if (r == 0) { // child will run
+    if (dup2(fd[0], fileno(stdin)) == -1) {
+      perror("dup2");
+      exit(1);
+    }
+    if (close(fd[1]) == -1) {
+      perror("close");
+      exit(1);
+    }
+    if (close(fd[0]) == -1) {
+      perror("close");
+      exit(1);
+    }
+    execl("./validate", "validate", NULL);
+  }
 
   return 0;
 }
